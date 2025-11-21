35fec2d4e472f16d152d70eef8bd5fb9af92d08c
Jakub Suchánsky <sucha35291@mot.sps-dopravni.cz>
2025-11-21 08:50:00 +0100

    a

---
+++ a.py
@@ -0,0 +1,26 @@
+list1 = [
+    "Ranní procházka",
+    "Přečíst kapitolu z knihy",
+    "Uvařit si čaj",
+    "Poslechnout novou písničku",
+    "Krátká rozcvička",
+    "Napsat tři věci k vděčnosti",
+    "Rychlý úklid",
+    "Vyzkoušet nový recept",
+    "Zavolat kamarádovi",
+    "Naučit se něco nového",
+    "Vyvětrat místnost",
+    "Pauza bez telefonu",
+    "Naplánovat další den",
+    "Krátká meditace",
+    "Podívat se na západ slunce",
+    "Něco nakreslit",
+    "Projít staré fotky",
+    "Dát si oblíbenou svačinu",
+    "Poslechnout podcast",
+    "Zapsat nový nápad"
+]
+list1.append("A")
+list1.pop(0)
+print(len(list1))
+print(list1)
